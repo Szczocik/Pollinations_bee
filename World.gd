9bbed44780a86ee9c63856468164d81ce724d96d
Mariusz Popa <popa.mariusz@gmail.com>
2022-08-16 09:46:58 -0400

    World script

--- World.gd
+++ World.gd
@@ -2,5 +2,14 @@ extends Node2D
 
 onready var music = $BGM
 onready var player = $Player
+onready var bgm_timer = $BGM_Timer
 
 
+func _ready():
+	stop_music()
+	
+
+func stop_music():
+	bgm_timer.stop()
+
+		
