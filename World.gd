2af143a4b5599ac1bdbaa2a93da830acdfbd6ffe
Mariusz Popa <popa.mariusz@gmail.com>
2022-08-18 10:01:00 -0400

    light sprite

--- World.gd
+++ World.gd
@@ -5,10 +5,12 @@ func _ready():
 	MusicController.play_music()
 	$MusicTimer.start()
 	
+	
 
 func _on_MusicTimer_timeout():
 	MusicController.turn_down_volume()
 	$MusicReset.start()
+	
 
 func _on_MusicReset_timeout():
 	MusicController.reset_volume()
