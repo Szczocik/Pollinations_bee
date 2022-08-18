dcb3ab2d5af8ea8b475a8444b3797e376049217f
Mariusz Popa <popa.mariusz@gmail.com>
2022-08-18 06:48:22 -0400

    turn_down_volume

--- SFX/MusicController.gd
+++ SFX/MusicController.gd
@@ -1,14 +1,17 @@
 extends Node
 
 var music = load("res://SFX/Chiptune_Adventures_1.ogg")
-onready var time = $MusicTimer
+
+
+func _ready():
+	pass
 	
 func play_music():
 	$Music.stream = music
 	$Music.play()
 	
 func turn_down_volume():
-	$Music.volume_db = -50
+	$Music.volume_db = -20
 	
 func reset_volume():
 	$Music.volume_db = 0
--- World.gd
+++ World.gd
@@ -2,5 +2,22 @@ extends Node2D
 
 
 func _ready():
-	MusicController.play_music(0.2)
+	MusicController.play_music()
+	$MusicTimer.start()
 	
+
+		
+	
+	
+
+func _on_MusicTimer_timeout():
+	MusicController.turn_down_volume()
+
+
+func _on_MusicReset_timeout():
+	MusicController.reset_volume()
+
+
+
+func _on_MusicPause_timeout():
+	pass # Replace with function body.
