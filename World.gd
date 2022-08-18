123994cac8ed411c44d2fb2c755c5fd8dced59c7
Mariusz Popa <popa.mariusz@gmail.com>
2022-08-18 04:26:46 -0400

    turn_down_volume

--- World.gd
+++ World.gd
@@ -1,5 +1,13 @@
 extends Node2D
-
+onready var music = $MusicTimer
+var music_timer = true
 
 func _ready():
 	MusicController.play_music()
+	if music.set_timer_process_mode(1):
+		MusicController.play_music()
+		music_timer = false
+	else:
+		MusicController.turn_down_volume()
+		music_timer = true
+		
