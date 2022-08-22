9d1efbe20e8330223122268842614070f1ab9f23
Mariusz Popa <popa.mariusz@gmail.com>
2022-08-22 04:53:27 -0400

    bee move

--- Player.gd
+++ Player.gd
@@ -1,25 +1,22 @@
 extends KinematicBody2D
 
-export (int) var min_move_speed = 100
-export (int) var max_move_speed = 350
-export (int) var stop_distance = 20
+export (int) var speed = 200
 
 
-func _process(_delta):
-	_look_at_mouse()
-	_move_to_mouse()
+func _physics_process(delta):
+	process_mouse()
 
 	
-func _look_at_mouse():
-	look_at(get_global_mouse_position())
-	rotation_degrees = rotation_degrees + 90
-	
-func _move_to_mouse():
-	if position.distance_to(get_global_mouse_position()) > stop_distance:
-		var direction = get_global_mouse_position() - position
-		var normalized_direction = direction.normalized()
-		var direction_distance = direction.length()
-		move_and_slide(normalized_direction * max(min_move_speed,min(max_move_speed, direction_distance)))
+func process_mouse():
+	var target = get_viewport().get_mouse_position().x
+	var bee_width = get_node("CollisionShape2D").shape.extents.x
+	var viewport_width = get_viewport().size.x
+	if position.x < target:
+		if not test_move(Transform2D(transform), Vector2(1,0)):
+			position.x = min(target, viewport_width - bee_width)
+	elif position.x > target:
+		if not test_move(Transform2D(transform), Vector2(-1,0)):
+			position.x = max(target, bee_width)
 
 
 func move_to_flouer():
