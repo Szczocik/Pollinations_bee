df243a352406708d8fb7ca90e17c678cca135b7f
Mariusz Popa <popa.mariusz@gmail.com>
2022-08-26 04:31:52 -0400

    detect player

--- World.gd
+++ World.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal flower_on(flower)
+signal player_on(position)
 
 onready var player = $Player
 onready var Stripes = $Stripes.get_children()
@@ -41,7 +43,11 @@ func check_player():
 		pass
 
 
-func _on_Strip_body_entered(body): return body
+func _on_Strip_body_entered(player):
+	if player:
+		emit_signal("flower_on", global_position)
+		print(player.global_position)
+	
 func _on_Strip1_body_entered(body): return body
 func _on_Strip2_body_entered(body): return body
 func _on_Strip3_body_entered(body): return body
@@ -52,3 +58,9 @@ func _on_Strip7_body_entered(body): return body
 func _on_Strip8_body_entered(body): return body
 func _on_Strip9_body_entered(body): return body
 
+
+
+func _on_Strip_area_entered(flower):
+	if flower:
+		emit_signal("flower_on", flower, global_position)
+		print(flower, flower.global_position)
