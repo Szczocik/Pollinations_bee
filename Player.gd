64f619860708e541e2c3e9df8d80468a454d5dd6
Mariusz Popa <popa.mariusz@gmail.com>
2022-08-29 05:48:03 -0400

    move player

--- Player.gd
+++ Player.gd
@@ -1,12 +1,13 @@
-extends KinematicBody2D
+extends Area2D
 
-class_name Player 
+class_name Player
 
 export (int) var min_move_speed = 100
 export (int) var max_move_speed = 250
 export (int) var stop_distance = 20
 
-	
+var strip = []
+
 
 func _physics_process(delta):
 	process_mouse()
@@ -19,10 +20,10 @@ func process_mouse():
 	var bee_width = get_node("CollisionShape2D").shape.extents.x
 	var viewport_width = get_viewport().size.x
 	if position.x < target:
-		if not test_move(Transform2D(transform), Vector2(1,0)):
+		#if not test_move(Transform2D(transform), Vector2(1,0)):
 			position.x = min(target, viewport_width - bee_width)
 	elif position.x > target:
-		if not test_move(Transform2D(transform), Vector2(-1,0)):
+		#if not test_move(Transform2D(transform), Vector2(-1,0)):
 			position.x = max(target, bee_width)
 
 func _look_at_mouse():
@@ -34,9 +35,16 @@ func _move_to_mouse():
 		var direction = get_global_mouse_position() - position
 		var normalized_direction = direction.normalized()
 		var direction_distance = direction.length()
-		move_and_slide(normalized_direction * max(min_move_speed,min(max_move_speed, direction_distance)))
+		#move_and_slide(normalized_direction * max(min_move_speed,min(max_move_speed, direction_distance)))
 		
 func pollition_effect():
 	$Pollination.emitting = true
 	
 
+
+
+func _on_Player_area_entered(area):
+	if area:
+		strip.push_front(area.name)
+		strip.remove(1)
+		print(strip)
