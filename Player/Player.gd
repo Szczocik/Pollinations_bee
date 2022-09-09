c2f6054c3479ff9b77b98762837ab3dd7464cbcc
Mariusz Popa <popa.mariusz@gmail.com>
2022-09-09 06:38:39 -0400

    Main Menu

--- Player/Player.gd
+++ Player/Player.gd
@@ -41,6 +41,8 @@ func _move_to_flower():
 
 func add_score():
 	score += 1
+	if score >= 1:
+		$MenuTimer.start()
 	
 func pollition_effect():
 	$Pollination.emitting = true
@@ -53,3 +55,7 @@ func _on_Timer_timeout():
 func _on_Pollination_Timer_timeout():
 	$Pollination/FlowerOK.play()
 	pollition_effect()
+
+
+func _on_MenuTimer_timeout():
+	get_tree().change_scene("res://GUI/MainMenu.tscn")
