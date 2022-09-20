b0b4d6d8630bcbe3228490e630efb1957a1a6437
Mariusz Popa <popa.mariusz@gmail.com>
2022-09-20 08:29:00 -0400

    boom effect

--- NPCs/Worm.gd
+++ NPCs/Worm.gd
@@ -14,3 +14,12 @@ func _on_Worm3_body_entered(body):
 	if body is Player:
 		get_tree().call_group("player", "add_point_target")
 		queue_free()
+
+func boom_effect():
+	$Boom.start()
+	
+
+func _on_Worm1_body_entered(body):
+	if body is Player:
+		boom_effect()
+		
