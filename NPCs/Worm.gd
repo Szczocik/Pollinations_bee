9efc69ac3afd110cb700776d0a7643e5c09e4565
Mariusz Popa <popa.mariusz@gmail.com>
2022-09-20 07:21:52 -0400

    add world2

--- NPCs/Worm.gd
+++ NPCs/Worm.gd
@@ -9,7 +9,6 @@ func _physics_process(delta):
 func _on_Worm2_body_entered(body):
 	if body is Player:
 		get_tree().call_group("player", "down_score")
-		queue_free()
 		
 func _on_Worm3_body_entered(body):
 	if body is Player:
